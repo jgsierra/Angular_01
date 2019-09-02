1bba8ebd3fddd619dd176f34a66bede66bbeb1d9
jgsierra <juangabrielsierra@gmail.com>
2019-09-02 18:26:50 -0300

    componet user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
+    XyzComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
