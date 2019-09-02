0dc473abce7d8bcb278def3765dfe10458e88c3b
jgsierra <juangabrielsierra@gmail.com>
2019-09-01 21:30:44 -0300

    practica ang

---
+++ src/app/xyz/xyz.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xyz',
+  templateUrl: './xyz.component.html',
+  styleUrls: ['./xyz.component.css']
+})
+export class XyzComponent implements OnInit {
+
+  titulo = '0 descensos';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
